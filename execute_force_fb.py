daccd95a7004126c546077cba02b0c3ed6ae7438
hehonglu123 <hehonglu123@126.com>
2024-01-24 19:57:14 -0500

    force load and traversal with results

--- execute_force_fb.py
+++ execute_force_fb.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import glob, cv2, sys, time
 from scipy.interpolate import CubicSpline
 from general_robotics_toolbox import *
+from copy import deepcopy
 sys.path.append('toolbox')
 from robot_def import *
 from lambda_calc import *
@@ -18,6 +19,11 @@ def spline_js(cartesian_path,curve_js,vd,rate=250):
 	lam=np.linspace(0,lam[-1],int(rate*lam[-1]/vd))
 	return polyfit(lam)
 
+def adjoint_map(T):
+	R=T.R
+	p=T.p
+	return np.vstack((np.hstack((R,np.zeros((3,3)))),np.hstack((hat(p)@R,R))))
+
 class MotionController(object):
 	def __init__(self,robot,ipad_pose,H_pentip2ati,controller_param):
 		
@@ -28,8 +34,10 @@ class MotionController(object):
 		self.ipad_pose_T = Transform(self.ipad_pose[:3,:3],self.ipad_pose[:3,-1])
 		self.H_pentip2ati=H_pentip2ati
 		self.H_ati2pentip=np.linalg.inv(H_pentip2ati)
+		self.ad_ati2pentip=adjoint_map(Transform(self.H_ati2pentip[:3,:3],self.H_ati2pentip[:3,-1]))
+		self.ad_ati2pentip_T=self.ad_ati2pentip.T
 		# controller parameters
-		self.param=controller_param
+		self.params=controller_param
 
 		####################################################FT Connection####################################################
 		RR_ati_cli=RRN.ConnectService('rr+tcp://localhost:59823?service=ati_sensor')
@@ -61,54 +69,119 @@ class MotionController(object):
 	
 	def wrench_wire_cb(self,w,value,time):
 
-		self.ft_reading = [value['force']['x'],value['force']['y'],value['force']['z'],value['torque']['x'],value['torque']['y'],value['torque']['z']]
+		self.ft_reading = np.array([value['torque']['x'],value['torque']['y'],value['torque']['z'],value['force']['x'],value['force']['y'],value['force']['z']])
 
 	def force_impedence_ctrl(self,f_err):
      
-		return self.param["force_ctrl_damping"]*f_err
+		return self.params["force_ctrl_damping"]*f_err
 
 	def force_load_z(self,fz_des):
 
+		# transform to tip desired
+		fz_des = fz_des*(-1)
+
 		touch_t = None
+		this_st=None
+		ft_record=[]
 		while True:
 			# force reading
-			ft_tip = self.H_ati2pentip@np.append(self.ft_reading[:3],1)
-			fz_now = ft_tip[2]
+			ft_tip = self.ad_ati2pentip_T@self.ft_reading
+			fz_now = float(ft_tip[-1])
 			# tool pose reading
 			q_now = self.robot_state.InValue.joint_position
 			tip_now = self.robot.fwd(q_now)
 			tip_now_ipad = self.ipad_pose_T.inv()*tip_now
-
+			# time force joint angle record
+			ft_record.append(np.append(np.array([time.time(),fz_now]),np.degrees(q_now)))
 			# force control
-			tip_next_ipad = Transform(tip_now_ipad.R,tip_now_ipad.p)
-			if fz_now < self.params['force_epsilon']: # if not touch ipad
+			tip_next_ipad = deepcopy(tip_now_ipad)
+			if np.abs(fz_now) < 0.5 and touch_t is None: # if not touch ipad
 				tip_next_ipad.p[2] = tip_now_ipad.p[2] + -1*self.params['load_speed'] * self.TIMESTEP # move in -z direction
 			else: # when touch ipad
 				if touch_t is None:
 					touch_t=time.time()
 				# track a trapziodal force profile
-				this_fz_des = min(fz_des,(time.time()-touch_t)*self.param["trapzoid_slope"])
+				this_fz_des = min(fz_des,(time.time()-touch_t)*self.params["trapzoid_slope"])
 				f_err = this_fz_des-fz_now # feedback error
 				v_des = self.force_impedence_ctrl(f_err) # force control
-				tip_next_ipad.p[2] = tip_now_ipad.p[2] + v_des * self.TIMESTEP # force impedence control
+				tip_next_ipad.p[2] = tip_now_ipad.p[2] + v_des * self.TIMESTEP # force impedence control 
 			
 			# check if force achieved
 			if np.fabs(fz_des-fz_now)<self.params['force_epsilon']:
-				if (time.time()-set_time)>self.parame['settling_time']:
+				if (time.time()-set_time)>self.params['settling_time']:
 					break
 			else:
 				set_time = time.time()
 
 			# get joint angles using ik
 			tip_next = self.ipad_pose_T*tip_next_ipad
+			# print(tip_next.p)
 			q_des = robot.inv(tip_next.p,tip_next.R,q_now)[0]
 			
 			# send robot position command
+			# if this_st is not None:
+			# 	while time.time()-this_st<self.TIMESTEP-0.00001:
+			# 		pass
+			# this_st=time.time()
 			self.position_cmd(q_des)
+
+			if np.linalg.norm(ft_tip[3:])>5:
+				print("force: ",ft_tip[3:])
+				print("force too large")
+				break
+		
+		return ft_record
    
-	def trajectory_force_control(self,q_all):
-     
-		pass
+	def trajectory_force_control(self,q_all,fz_des):
+
+		# transform to tip desired
+		fz_des = fz_des*(-1)
+    
+		lam = calc_lam_js(q_all,self.robot)
+
+		time_bp=np.array(lam)/self.params['moveL_speed_lin']
+		seg=1
+
+		start_time=time.time()
+		dt=0.004
+		st=None
+		ft_record=[]
+		while time.time()-start_time<time_bp[-1]:
+			if st is not None:
+				dt = time.time()-st
+			st=time.time()
+			# force reading
+			ft_tip = self.ad_ati2pentip_T@self.ft_reading
+			fz_now = float(ft_tip[-1])
+			f_err = fz_des-fz_now # feedback error
+			v_des_z = self.force_impedence_ctrl(f_err) # force control
+			if np.linalg.norm(ft_tip[3:])>5: # force protection break
+				print("force: ",ft_tip[3:])
+				print("force too large")
+				break
+			# joint reading
+			q_now = self.robot_state.InValue.joint_position
+			tip_now = self.robot.fwd(q_now)
+			tip_now_ipad = self.ipad_pose_T.inv()*tip_now
+			# time force joint angle record
+			ft_record.append(np.append(np.array([time.time(),fz_now]),np.degrees(q_now)))
+
+			#find current segment
+			if time.time()-start_time>time_bp[seg]:
+				seg+=1
+			if seg==len(q_all):
+				break
+			frac=(time.time()-start_time-time_bp[seg-1])/(time_bp[seg]-time_bp[seg-1])
+			q_planned=frac*q_all[seg]+(1-frac)*q_all[seg-1]
+			T_planned=self.robot.fwd(q_planned)
+			T_planned_ipad=self.ipad_pose_T.inv()*T_planned
+			T_planned_ipad.p[2] = tip_now_ipad.p[2] + v_des_z * dt # force impedence control
+			T_planned=self.ipad_pose_T*T_planned_ipad
+			q_des = self.robot.inv(T_planned.p,T_planned.R,q_now)[0]
+
+			self.position_cmd(q_des)
+		
+		return ft_record
 	
 	def jog_joint_position_cmd(self,q,v=0.4,wait_time=0):
 
@@ -169,19 +242,22 @@ def main():
 	num_segments=len(glob.glob('path/cartesian_path/'+img_name+'/*.csv'))
 	robot=robot_obj('ABB_1200_5_90','config/ABB_1200_5_90_robot_default_config.yml',tool_file_path='config/heh6_pen.csv')
 	# robot=robot_obj('ur5','config/ur5_robot_default_config.yml',tool_file_path='config/heh6_pen_ur.csv')
+	# print(robot.fwd(np.array([0,0,0,0,0,0])))
+	# exit()
 
 	######## FT sensor info
 	H_pentip2ati=np.loadtxt('config/pentip2ati.csv',delimiter=',')
  
 	######## Controller parameters ###
 	controller_params = {
-        "force_ctrl_damping": 100.0,
-        "force_epsilon": 0.5, # Unit: N
-        "moveL_speed_lin": 25.0, # Unit: mm/sec
+        "force_ctrl_damping": 40.0,
+        "force_epsilon": 0.1, # Unit: N
+        "moveL_speed_lin": 10.0, # Unit: mm/sec
         "moveL_speed_ang": np.radians(10), # Unit: rad/sec
-        "trapzoid_slope": 10, # trapzoidal load profile. Unit: N/sec
-        "load_speed": 20.0, # Unit mm/sec
-        "unload_speed": 1.0 # Unit mm/sec
+        "trapzoid_slope": 1, # trapzoidal load profile. Unit: N/sec
+        "load_speed": 50.0, # Unit mm/sec
+        "unload_speed": 1.0, # Unit mm/sec
+		'settling_time': 1 # Unit: sec
         }
 	
 	######## Motion Controller ###
@@ -189,9 +265,11 @@ def main():
 	mctrl.connect_position_mode()
 
 	F_MAX=0	#maximum pushing force 10N
-	F_des = 2 # desired force = 2N
+	F_des = 2 # desired force unit: N
 
 	start=True
+	ft_record_load=[]
+	ft_record_move=[]
 	for i in range(num_segments):
 		pixel_path=np.loadtxt('path/pixel_path/'+img_name+'/%i.csv'%i,delimiter=',').reshape((-1,3))
 		cartesian_path=np.loadtxt('path/cartesian_path/'+img_name+'/%i.csv'%i,delimiter=',').reshape((-1,3))
@@ -201,15 +279,16 @@ def main():
 
 			pose_start=robot.fwd(curve_js[0])
 			if start:
+				h_offset = 10
 				#jog to starting point
-				p_start=pose_start.p+30*ipad_pose[:3,-2]
+				p_start=pose_start.p+h_offset*ipad_pose[:3,-2]
 				q_start=robot.inv(p_start,pose_start.R,curve_js[0])[0]
 				mctrl.jog_joint_position_cmd(q_start,wait_time=0.5)
-				mctrl.force_load_z(F_des)
+				ft_record = mctrl.force_load_z(F_des)
 				# clear bias
 				start=False
 			else:
-				h_offset = 20
+				h_offset = 10
     			#arc-like trajectory to next segment
 				p_start=pose_start.p+h_offset*ipad_pose[:3,-2]
 				q_start=robot.inv(p_start,pose_start.R,curve_js[0])[0]
@@ -217,50 +296,32 @@ def main():
 				p_mid=(pose_start.p+pose_cur.p)/2+h_offset*ipad_pose[:3,-2]
 				q_mid=robot.inv(p_mid,pose_start.R,curve_js[0])[0]
 				
-				mctrl.trajectory_position_cmd(np.vstack((robot_state.InValue.joint_position,q_mid,q_start)),v=0.2)
+				mctrl.trajectory_position_cmd(np.vstack((mctrl.robot_state.InValue.joint_position,q_mid,q_start)),v=0.2)
 				mctrl.jog_joint_position_cmd(q_start,wait_time=0.3)
-				mctrl.force_load_z(F_des)
+				ft_record=mctrl.force_load_z(F_des)
+			
+			ft_record_load.append(ft_record)
 
 			traversal_velocity=50
 
-			###force feedback, traj tracking QP
-			pose_cur=robot.fwd(robot_state.InValue.joint_position)
-			q_cmd=curve_js[0]
-			ts=time.time()
-			for m in range(1,len(curve_js)):
-				# v_d=cartesian_path[m]-cartesian_path[m-1]
-				# v_d=v_d/np.linalg.norm(v_d)
-				f_d=F_MAX*pixel_path[m,-1]
-				while np.linalg.norm(pose_cur.p[:2]-cartesian_path[m,:2])>2:	###loop to get to waypoint
-					v_d=cartesian_path[m]-pose_cur.p
-					if np.linalg.norm(v_d)>2.5:
-						v_d=traversal_velocity*v_d/np.linalg.norm(v_d)
-					# print('v_d: ',v_d)
-					#force feedback
-					res, tf, status = ati_tf.try_read_ft_streaming(.1)###get force feedback
-					f_cur=force_prop(tf,H_pentip2ati[:-1,-1])
-					print(f_cur)
-					# position_gain=0.01
-					position_gain=0.0
-					v_d+=position_gain*(f_d-f_cur)*(-ipad_pose[:3,-2])
-					p_inc=(time.time()-ts)*v_d	###discrete increments in dt
-					# print('p_inc: ',p_inc)
-					dq=np.linalg.pinv(robot.jacobian(robot_state.InValue.joint_position))@np.hstack((np.zeros(3),p_inc))
-					q_cmd+=dq
-					position_cmd(q_cmd)
+			#drawing trajectory
+			print("Travel trajectory")
+			ft_record=mctrl.trajectory_force_control(curve_js,F_des)
+			ft_record_move.append(ft_record)
 
-					pose_cur=robot.fwd(robot_state.InValue.joint_position)
-					ts=time.time()
-
-				
-			#jog to end point
-			jog_joint_position_cmd(curve_js[-1],wait_time=0.3)
+			mctrl.jog_joint_position_cmd(q_start,wait_time=0.5)
 	
 	#jog to end point
 	pose_end=robot.fwd(curve_js[-1])
 	p_end=pose_end.p+20*ipad_pose[:3,-2]
 	q_end=robot.inv(p_end,pose_end.R,curve_js[-1])[0]
-	jog_joint_position_cmd(q_end)
+	mctrl.jog_joint_position_cmd(q_end)
+
+	for i in range(len(ft_record_load)):
+		ft_record_load[i]=np.array(ft_record_load[i])
+		ft_record_move[i]=np.array(ft_record_move[i])
+		np.savetxt('record/ft_record_load_%i.csv'%i,ft_record_load[i],delimiter=',')
+		np.savetxt('record/ft_record_move_%i.csv'%i,ft_record_move[i],delimiter=',')
 
 if __name__ == '__main__':
 	main()
